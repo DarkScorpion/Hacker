d707f3740064075f084cadd6e1efbb570a974ea2
Scorpion <dscorpion@yandex.ru>
2014-05-26 12:59:34 +0800

    convert tabs into spaces

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ app.listen(process.env.VCAP_APP_PORT || 3000);
 
 app.get('/', function(req, res)
 {
-	res.render('hacker.jade');
-	sp.i('Open title page from ' + sp.getClientIP(req));
+    res.render('hacker.jade');
+    sp.i('Open title page from ' + sp.getClientIP(req));
 });
 
 app.get('/girl=:girlName;:imageUrl;', function(req, res) 
 {
-	sp.i('Open girls page!');
-	res.render('girl.jade', 
+    sp.i('Open girls page!');
+    res.render('girl.jade', 
     {
-		gName: req.params.girlName,
-		iUrl: req.params.imageUrl
+        gName: req.params.girlName,
+        iUrl: req.params.imageUrl
     });
 });
 
 app.get('/id=:id([0-9]+)', function(req, res) 
 {
-	res.send(req.params.id);
-	sp.i('id='+req.params.id);
+    res.send(req.params.id);
+    sp.i('id='+req.params.id);
 });
 
 app.get('/name=:name;:last;', function(req, res)
 {
     res.send('Hello '+req.params.name+' '+req.params.last);
-	sp.i('Name='+req.params.name+' Last='+req.params.last);
+    sp.i('Name='+req.params.name+' Last='+req.params.last);
 });
 
 app.get('*', function(req, res) 
 {
     res.render('error404.jade');
-	sp.i('Wrong params: '+req.params[0]);
+    sp.i('Wrong params: '+req.params[0]);
 });
 
 sp.i('NodeJS is runing');
--- suplib.js
+++ suplib.js
@@ -1,19 +1,19 @@
 exports.i = function (str) //i = info
 {
-	console.log(iTime()+' '+str);
+    console.log(iTime()+' '+str);
 }
 
 exports.d = function (str) //d = debug
 {
-	console.log(dTime()+' '+str);
+    console.log(dTime()+' '+str);
 }
 
 exports.sms = function (id, number, text)
 {
-	//TODO
-	console.log (iTime()+' Sms is send:'+
-		'\n\tnumber: '+number+
-		'\n\tmessage: '+text);
+    //TODO
+    console.log (iTime()+' Sms is send:'+
+        '\n\tnumber: '+number+
+        '\n\tmessage: '+text);
 }
 
 exports.getClientIP = function (req)
@@ -26,15 +26,15 @@ exports.getClientIP = function (req)
 function iTime()
 {
     var d = new Date();
-	s = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
-	t = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+    s = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
+    t = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
     return '['+s+' '+t+']';
 }
 
 function dTime()
 {
     var d = new Date();
-	s = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
-	t = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()+':'+d.getMilliseconds();
+    s = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
+    t = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()+':'+d.getMilliseconds();
     return '['+s+' '+t+']';
 }
