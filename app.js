3aee5dcf337957b604c64e64e693344a356e1629
Scorpion <dscorpion@yandex.ru>
2014-05-28 15:41:42 +0800

    add title in hacker page

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var sp = require('./suplib'); //connect support library
 app.set('views/', __dirname); //for Jade rendering
 app.use(express.favicon('web/console.ico')); //icon of site
 app.use(express.static(__dirname + '/web')); //for css and js files
-//app.use(express.basicAuth('a_name', 'A_PASS')); //authorization connect
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
 
