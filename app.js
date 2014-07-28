1d0135c1b487801d659db824ce1cd95ad95c0251
Scorpion <dscorpion@yandex.ru>
2014-07-28 10:45:03 +0800

    tabs4 -> spaces2

--- app.js
+++ app.js
@@ -10,49 +10,49 @@ app.listen(process.env.VCAP_APP_PORT || 3000);
 
 app.get('/', function(req, res)
 {
-    res.render('hacker.jade');
-    sp.i('Open title page from ' + sp.getClientIP(req));
+  res.render('hacker.jade');
+  sp.i('Open title page from ' + sp.getClientIP(req));
 });
 
 app.get('/ajax', function(req, res)
 {
-    res.render('ajax.jade');
-    sp.i('Ajax page open');
+  res.render('ajax.jade');
+  sp.i('Ajax page open');
 });
 
 app.get('/api', function(req, res)
 {
-    res.send(req);
-    sp.i('Ajax reqwest: ' + req);
+  res.send(req);
+  sp.i('Ajax reqwest: ' + req);
 });
 
 
 app.get('/id=:id([0-9]+)', function(req, res) 
 {
-    res.send(req.params.id);
-    sp.i('id='+req.params.id);
+  res.send(req.params.id);
+  sp.i('id='+req.params.id);
 });
 
 app.get('/name=:name;:last;', function(req, res)
 {
-    res.send('Hello '+req.params.name+' '+req.params.last);
-    sp.i('Name='+req.params.name+' Last='+req.params.last);
+  res.send('Hello '+req.params.name+' '+req.params.last);
+  sp.i('Name='+req.params.name+' Last='+req.params.last);
 });
 
 app.get('/girl=:girlName;:imageUrl;', function(req, res) 
 {
-    sp.i('Open girls page!');
-    res.render('girl.jade', 
-    {
-        gName: req.params.girlName,
-        iUrl: req.params.imageUrl
-    });
+  sp.i('Open girls page!');
+  res.render('girl.jade', 
+  {
+    gName: req.params.girlName,
+    iUrl: req.params.imageUrl
+  });
 });
 
 app.get('*', function(req, res) 
 {
-    res.render('error404.jade');
-    sp.i('Wrong params: '+req.params[0]);
+  res.render('error404.jade');
+  sp.i('Wrong params: '+req.params[0]);
 });
 
 sp.i('NodeJS is runing');
