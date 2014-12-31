e200898a2461d2f9afa2bce6f042868af70d9e8c
Alex Smith <iqwz@ya.ru>
2014-12-31 12:01:29 +0900

    use modele serve-favicon

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express'),
     app = express(), //create server Express
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    favicon = require('serve-favicon');
 
 var sp = require('./suplib'); //connect support library
 
 //app.use(express.logger());
 app.set('views/', __dirname); //for jade rendering
+app.use(favicon('web/other/console.ico')); //icon of site
 app.use(bodyParser.urlencoded({ extended: false })); //for ajax json
-//app.use(express.favicon('web/other/console.ico')); //icon of site
 app.use(express.static(__dirname + '/web')); //for css and js files
 
 app.get('/', function(req, res)
